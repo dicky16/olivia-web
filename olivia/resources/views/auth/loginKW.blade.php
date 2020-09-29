e6fa1620b73feb479c9c821202a7120523f880ea
djavu <setyawandicky88@gmail.com>
2020-09-29 19:38:33 +0700

    ganti struktur folder

--- olivia/resources/views/auth/loginKW.blade.php
+++ olivia/resources/views/auth/loginKW.blade.php
@@ -1,86 +1,107 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Login V3</title>
+	<title>Login V18</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="{{ asset('auth/images/icons/favicon.ico') }}"/>
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{ asset('auth/vendor/bootstrap/css/bootstrap.min.css') }}">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{ asset('auth/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('auth/fonts/iconic/css/material-design-iconic-font.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('auth/fonts/Linearicons-Free-v1.0.0/icon-font.min.css') }}">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{ asset('auth/vendor/animate/animate.css') }}">
-<!--===============================================================================================-->
+<!--===============================================================================================-->	
 	<link rel="stylesheet" type="text/css" href="{{ asset('auth/vendor/css-hamburgers/hamburgers.min.css') }}">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{ asset('auth/vendor/animsition/css/animsition.min.css') }}">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{ asset('auth/vendor/select2/select2.min.css') }}">
-<!--===============================================================================================-->
+<!--===============================================================================================-->	
 	<link rel="stylesheet" type="text/css" href="{{ asset('auth/vendor/daterangepicker/daterangepicker.css') }}">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{ asset('auth/css/util.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('auth/css/main.css') }}">
 <!--===============================================================================================-->
 </head>
-<body>
-
+<body style="background-color: #666666;">
+	
 	<div class="limiter">
-		<div class="container-login100" style="background-image: url('{{ asset('auth/images/bg-01.jpg') }}');">
+		<div class="container-login100">
 			<div class="wrap-login100">
 				<form class="login100-form validate-form" method="POST" action="{{ route('login') }}">
-					<span class="login100-form-logo">
-						<i class="zmdi zmdi-landscape"></i>
+				@csrf
+					<span class="login100-form-title p-b-43">
+						Login to continue
 					</span>
-
-					<span class="login100-form-title p-b-34 p-t-27">
-						Log in
-					</span>
-            @csrf
-					<div class="wrap-input100 validate-input" data-validate = "Enter username">
-						<input class="input100" id="email" type="email" name="email" placeholder="Username">
-						<span class="focus-input100" data-placeholder="&#xf207;"></span>
+					
+					
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input id="email" type="text" name="email" placeholder="Email" class="input100 form-control @error('email') is-invalid @enderror">
+						@error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+					</div>
+					
+					
+					<div class="wrap-input100 validate-input" data-validate="Password is required">
+						<input class="input100" type="password" name="pass" placeholder="Password">
 					</div>
 
-					<div class="wrap-input100 validate-input" data-validate="Enter password">
-						<input class="input100" id="password" type="password" name="password" placeholder="Password">
-						<span class="focus-input100" data-placeholder="&#xf191;"></span>
-					</div>
+					<div class="flex-sb-m w-full p-t-3 p-b-32">
+						<div class="contact100-form-checkbox">
+							<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+							<label class="label-checkbox100" for="ckb1">
+								Remember me
+							</label>
+						</div>
 
-					<div class="contact100-form-checkbox">
-						<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
-						<label class="label-checkbox100" for="ckb1">
-							Remember me
-						</label>
+						<div>
+							<a href="#" class="txt1">
+								Forgot Password?
+							</a>
+						</div>
 					</div>
+			
 
 					<div class="container-login100-form-btn">
-						<!-- <button type="submit" class="login100-form-btn">
-							Login
-						</button> -->
-            <button type="submit" class="login100-form-btn">
-                {{ __('Login') }}
-            </button>
-
+						<button class="login100-form-btn">
+							{{ __('Login') }}
+						</button>
+					</div>
+					
+					<!-- <div class="text-center p-t-46 p-b-20">
+						<span class="txt2">
+							or sign up using
+						</span>
 					</div>
 
-					<div class="text-center p-t-90">
-						<a class="txt1" href="#">
-							Forgot Password?
+					<div class="login100-form-social flex-c-m">
+						<a href="#" class="login100-form-social-item flex-c-m bg1 m-r-5">
+							<i class="fa fa-facebook-f" aria-hidden="true"></i>
 						</a>
-					</div>
+
+						<a href="#" class="login100-form-social-item flex-c-m bg2 m-r-5">
+							<i class="fa fa-twitter" aria-hidden="true"></i>
+						</a>
+					</div> -->
 				</form>
+
+				<div class="login100-more" style="background-image: url({{ asset('auth/images/bg-01.jpg') }});">
+				</div>
 			</div>
 		</div>
 	</div>
+	
+	
 
-
-	<div id="dropDownSelect1"></div>
-
+	
+	
 <!--===============================================================================================-->
 	<script src="{{ asset('auth/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
 <!--===============================================================================================-->
@@ -96,7 +117,7 @@
 <!--===============================================================================================-->
 	<script src="{{ asset('auth/vendor/countdowntime/countdowntime.js') }}"></script>
 <!--===============================================================================================-->
-	<script src="{{ asset('auth/js/main.js') }}"></script>
+	<script src="{{ asset('authjs/main.js') }}"></script>
 
 </body>
 </html>
