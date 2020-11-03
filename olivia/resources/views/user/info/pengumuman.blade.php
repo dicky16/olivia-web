0ec81da307c87163b9c3e48e8d5de150b04c9336
djavu <setyawandicky88@gmail.com>
2020-11-03 19:27:25 +0700

    ubah pengumuman

--- olivia/resources/views/user/info/pengumuman.blade.php
+++ olivia/resources/views/user/info/pengumuman.blade.php
@@ -24,30 +24,29 @@
     <section class="blog-area section_padding_100">
         <div class="container">
             <div class="row">
-                <div class="col-12 col-md-8">
+            <div class="col-12 col-md-8">
                     <div class="olv-blog-posts">
                         <div class="row">
-                            @foreach($data as $berita)
+                            @foreach($data as $pengumuman)
                             <?php
-                                date_default_timezone_set("Asia/Jakarta");
-                                $desArr = explode(".", $berita->deskripsi);
+                                $desArr = explode(".", $pengumuman->deskripsi);
                             ?>
                             <div class="col-12">
                                 <div class="single-blog wow fadeInUp" data-wow-delay="0.2s">
                                     <!-- Post Thumb -->
                                     <div class="blog-post-thumb">
-                                        <img src="{{ url('') }}/{{$berita->foto}}" alt="">
+                                        <img src="{{ url('') }}/{{$pengumuman->gambar}}">
                                     </div>
                                     <!-- Post Meta -->
                                     <div class="post-meta">
-                                        <h6>By <a href="#">{{ $berita->name }}, </a><a href="#">{{date('d F Y', strtotime($berita->created_at))}}</a></h6>
+                                        <h6>By <a href="#">{{ $pengumuman->name }}, </a><a href="#">{{ date('d F Y', strtotime($pengumuman->created_at)) }}</a></h6>
                                     </div>
                                     <!-- Post Title -->
-                                    <h2>{{$berita->judul}}</h2>
+                                    <h2>{{$pengumuman->judul}}</h2>
                                     <!-- Post Excerpt -->
-                                    <p>{!! $desArr[0] !!}. {!!$desArr[1]!!}</p>
+                                    <p>{!! $desArr[0] !!}. {!! $desArr[1] !!}</p>
                                     <!-- Read More btn -->
-                                    <a href="{{ url('berita') }}/{{$berita->id}}">Read More</a>
+                                    <a href="{{ url('pengumuman') }}/{{$pengumuman->id}}">Read More</a>
                                 </div>
                             </div>
                             @endforeach
@@ -58,7 +57,6 @@
                     <div class="olv-pagination-area">
                         <nav>
                             <ul class="pagination">
-                            
                                 {{ $data->links() }}
                             </ul>
                         </nav>
@@ -70,13 +68,12 @@
                         <div class="latest-blog-posts mb-100">
                             <h5>Latest Posts</h5>
                             @foreach($latest as $terbaru)
-                            
                             <div class="single-latest-blog-post d-flex">
                                 <div class="latest-blog-post-thumb">
-                                    <img src="{{ url('') }}/{{$terbaru->foto}}" alt="">
+                                    <img src="{{ url('') }}/{{$terbaru->gambar}}" alt="">
                                 </div>
                                 <div class="latest-blog-post-content">
-                                    <h6><a href="{{ url('berita') }}/{{$berita->id}}">{{ $terbaru->judul}}</a></h6>
+                                    <h6><a href="#">{{ $terbaru->judul}}</a></h6>
                                     <div class="post-meta">
                                         <h6>By <a href="">{{ $terbaru->name }}, </a>/<a href="#"> {{ date('d F Y', strtotime($terbaru->created_at)) }}</a></h6>
                                     </div>
